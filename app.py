f64de1b0d816539674fc8dcdc3034ada3a457187
thilohuellmann <thilo.huellmann@whu.edu>
2018-02-17 20:01:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 # Flask
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# Index
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+    app.secret_key = os.environ['APP_SECRET_KEY']
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
